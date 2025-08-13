0ca977837fdf5d3b72eebb35d2609361ee75d144
Yang Liu <liu-yang21@mails.tsinghua.edu.cn>
2025-08-13 17:33:04 +0800

    commit 13

---
+++ app/posts/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from 'next';
+import SectionContainer from '@/components/common/SectionContainer';
+import PageHeader from '@/components/common/PageHeader';
+import CertificateGrid from '@/components/posts/CertificateGrid';
+
+export const metadata: Metadata = {
+	title: 'Posts | Zhuoya Cao',
+	description: 'View posts by Zhuoya Cao as a young scholar.',
+};
+
+export default function CertificatesPage() {
+	return (
+		<div className="pt-24">
+			<SectionContainer>
+				<PageHeader
+					title="Posts"
+					description="My vivid record about my academic journey."
+				/>
+
+				<CertificateGrid />
+			</SectionContainer>
+		</div>
+	);
+}
