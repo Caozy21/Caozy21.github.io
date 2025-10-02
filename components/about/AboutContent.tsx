868888deb5b79bf30aa9b6a1528c95496e0c3b71
Yang Liu <liu-yang21@mails.tsinghua.edu.cn>
2025-10-01 17:10:51 -0700

    commit 18

--- components/about/AboutContent.tsx
+++ components/about/AboutContent.tsx
@@ -43,11 +43,26 @@ export default function AboutContent() {
 					Hello! I'm Zhuoya Cao, currently a research assistant in the Division of Astronomy at Westlake University, China. Prior to this, I obtained my B.Sc. from Tsinghua University, China, majoring in Mechanics and minoring in Astronomy.
 				</p>
 
-				<p className="text-muted-foreground">
+				{/* <p className="text-muted-foreground">
 					My research interest lies in dynamics of planetary systems, including rich interactions between a planet and other planets, stellar flybys, and protoplanetary disks. I am currently working with Prof. Shude Mao and Prof. Douglas N.C. Lin on simulations of planetary systems.
+				</p> */}
+
+				<p className="text-muted-foreground">
+					My fate with astronomy began in junior high years with the microfilm of "Journal to the Stars", a story about a boy chasing his dream of becoming an astrophysicist. I was fascinated by the world inside the telescope that most people may never see or imagine. I did not have the chance to buy a telescope then, but the starry night sky somehow rooted astrophysics in my heart. I went for physics Olympics in high school, and was later studying mechanics in Tsinghua with astronomy as my minor. 
 				</p>
 
-				<div className="grid grid-cols-2 gap-4 my-8">
+				<p className="text-muted-foreground">
+					In my undergraduate years, I participated in multiple research projects from N-body dynamics in solar system to hydrodynamics in protoplanetary disks -- enjoying a taste combining mechanics and astronomy contexts. I awed at how most basic mechanics principles could be applied to understand the complex dynamics of solar system, Oort cloud and protoplanetary disks.
+				</p>
+
+				<p className="text-muted-foreground">
+					Thanks to my brilliant advisors and precious fellowship, I had the opportunity to intern at Harvard Center for Astrophysics in 2024 fall with Prof. Avi Loeb, and do research assistantship at Westlake University in 2025 with Prof. Shude Mao and Prof. Doug Lin. I also went to the ET 2.0 Conference, Shanghai to present my work on Oort cloud dynamics. These experiences helped me to see a broader astronomy society beyond my computer and sharpen my research skills as a young astronomer.
+				</p>
+
+				<p className="text-muted-foreground">
+					In my part time, I am a fan of singing, nice food and traveling. I am also a beginner in photography and Japanese. I enjoy exploring new things and challenging myself.
+				</p>
+				{/* <div className="grid grid-cols-2 gap-4 my-8">
 					<div className="flex flex-col items-center bg-card p-4 rounded-lg shadow-sm">
 						<Flask className="h-10 w-10 text-chart-1 mb-2" />
 						<h3 className="font-medium">Research</h3>
@@ -64,7 +79,7 @@ export default function AboutContent() {
 						<Heart className="h-10 w-10 text-chart-4 mb-2" />
 						<h3 className="font-medium">Passion</h3>
 					</div>
-				</div>
+				</div> */}
 
 				<p className="text-muted-foreground">
 					I'm currently seeking graduate positions to apply my knowledge and skills in a professional research environment where I can continue to grow and make meaningful contributions to the field of astronomy.
