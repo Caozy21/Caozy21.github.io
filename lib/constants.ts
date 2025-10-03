9b9687ad7dd4a8fd135a37f3ea5d347966a307ec
Yang Liu <liu-yang21@mails.tsinghua.edu.cn>
2025-10-02 19:12:35 -0700

    commit 25

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,8 +2,8 @@ export const projects = [
 	{
 		id: 1,
 		title: "Migration of High-eccentricity Gas Giants on a Protoplanetary Disk",
-		description: "Conducting hydrodynamic simulations on gas giant migration on a protoplanetary disk by Athena++. Integrating migration laws from hydro simulations into REBOUNDx framework for effective dynamic simulation on complex planetary systems.",
-		imageUrl: "https://images.pexels.com/photos/2280549/pexels-photo-2280549.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		description: "Conducting hydrodynamic simulations on gas giant migration on a protoplanetary disk by Athena++. Integrating migration laws from hydro simulations into REBOUNDx framework for effective dynamic simulation on complex planetary systems. Image credit: spacedaily.com.",
+		imageUrl: "./stellar_flyby.jpg",
 		date: "Jun 2025 - Present",
 		collaborators: ["Prof. Shude Mao", "Prof. Shigeru Ida","Dr. Yaping Li"],
 		link: "#"
@@ -11,17 +11,17 @@ export const projects = [
 	{
 		id: 2,
 		title: "Launching Mechanism for FFPs from Host Stars with Cometary Companions and Close-in Planets",
-		description: "Conducted REBOUNDx simulation to test the influence of tides on the production of FFPs from the host stars with cometary companions and close-in planets. Deduced a theoretical model to explain the tide influence on such systems.",
-		imageUrl: "https://images.pexels.com/photos/3735218/pexels-photo-3735218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-		date: "May 2025 - Present",
+		description: "Conducted REBOUNDx simulation to test the influence of tides on the production of FFPs from the host stars with cometary companions and close-in planets. Deduced a theoretical model to explain the tide influence on such systems. Image credit: the European Southern Observatory.",
+		imageUrl: "./free_floating.jpg",
+		date: "May 2025 - Sep 2025",
 		collaborators: ["Prof. Douglas N.C. Lin", "Prof. Shigeru Ida", "Dr. Xiaochen Zheng"],
 		link: "#"
 	},
 	{
 		id: 3,
 		title: "Planet Migration on a Protoplanetary Disk with Ring-shaped Dead Zone",
-		description: "Conducted simulations on migration of planets on a protoplanetary disk with ring shaped dead zone by Athena++. Reveal xxx as a possible mechanism to stop gas giant migration.",
-		imageUrl: "https://images.pexels.com/photos/5726837/pexels-photo-5726837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		description: "Conducted simulations on migration of planets on a protoplanetary disk with ring shaped dead zone by Athena++. Reveal xxx as a possible mechanism to stop gas giant migration. Image credit: Center for Astrophysics | Harvard & Smithsonian.",
+		imageUrl: "./protoplanetary_disk.jpeg",
 		date: "May 2025 - Present",
 		collaborators: ["Prof. Douglas N.C. Lin", "Dr. Yaping Li"],
 		link: "#"
@@ -29,8 +29,8 @@ export const projects = [
 	{
 		id: 4,
 		title: "Comet Shower at the Pliocene-Pleistocene Transition Triggered by a Stellar Fly-by",
-		description: "Conducted numerous simulations on Oort cloud and stellar flyby by REBOUND package, revealed and explained the Oort cloud behaviors during single and binary star invasions. Theoretically explained a possible comet shower caused by HD 7977’s flyby 3 Myr ago, analyzed comet duration, intensity, and its impact on the ancient Earth. Collected geographic evidence of craters and comet components, revealing that this comet shower could be the cause of the Pliocene-Pleistocene Transition.",
-		imageUrl: "https://images.pexels.com/photos/2280571/pexels-photo-2280571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		description: "Conducted numerous simulations on Oort cloud and stellar flyby by REBOUND package, revealed and explained the Oort cloud behaviors during single and binary star invasions. Theoretically explained a possible comet shower caused by HD 7977’s flyby 3 Myr ago, analyzed comet duration, intensity, and its impact on the ancient Earth. Collected geographic evidence of craters and comet components, revealing that this comet shower could be the cause of the Pliocene-Pleistocene Transition. Image credit: AAAS | Science.",
+		imageUrl: "./oort.jpg",
 		date: "Sep 2024 - Feb 2025",
 		collaborators: ["Prof. Abraham Loeb", "Dr. Morgan Macleod"],
 		link: "#"
@@ -38,8 +38,8 @@ export const projects = [
 	{
 		id: 5,
 		title: "Stability of the Solar System by Impacts from Free Floating Planets to Stellar Flybys",
-		description: "Explored flybys' impact on planetary systems (solar system) by REBOUND, computatively and theoretically revealed the relationship between system collapse probability and intrusion perihelion distance on a large parameter space.",
-		imageUrl: "https://images.pexels.com/photos/906150/pexels-photo-906150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		description: "Explored flybys' impact on planetary systems (solar system) by REBOUND, computatively and theoretically revealed the relationship between system collapse probability and intrusion perihelion distance on a large parameter space. Photo: NASA Hubble Telescope. Image credit: solarsystemandgalaxy3.blogspot.com.",
+		imageUrl: "./solar_system.jpg",
 		date: "Oct 2023 - Jun 2024",
 		collaborators: ["Prof. Shude Mao"],
 		link: "#"
@@ -48,7 +48,7 @@ export const projects = [
 		id: 6,
 		title: "HST to JWST Super-Resolution Based on ControlNet",
 		description: "Using convolution techniques to down-resolve the JWST images to the HST resolution, and validating the database using ControlNet-based machine learning.",
-		imageUrl: "https://images.pexels.com/photos/5472310/pexels-photo-5472310.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		imageUrl: "./galaxy.jpg",
 		date: "Feb 2025 - Jun 2025",
 		collaborators: ["Prof. Zheng Cai", "Fujiang Yu"],
 		link: "#"
